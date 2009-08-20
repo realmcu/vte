8beedf7901de8cc48b725aad07d4b7b42cdaadfe
root <root@localhost.localdomain>
2009-08-20 14:14:30 +0800

    add new rtc test

---
+++ testcases/vte_tests_suite/rtc_tests/rtc_testapp_7/rtc_main.c
@@ -0,0 +1,284 @@
+/***
+ * **Copyright 2008-2009 Freescale Semiconductor, Inc. All Rights Reserved.
+ * **
+ * **The code contained herein is licensed under the GNU General Public
+ * **License. You may obtain a copy of the GNU General Public License
+ * **Version 2 or later at the following locations:
+ * **
+ * **http://www.opensource.org/licenses/gpl-license.html
+ * **http://www.gnu.org/copyleft/gpl.html
+ * **/
+/*================================================================================================*/
+/**
+    @file   rtc_main.c
+
+    @brief  RTC test 2 main file
+*/
+/*
+Revision History:
+                     Modification     Tracking
+Author                    Date          Number    Description of Changes
+-------------------------   ------------    ----------  -------------------------------------------
+Blake               12/29/2008
+====================================================================================================
+Portability:  ARM GCC  gnu compiler
+==================================================================================================*/
+
+/*==================================================================================================
+Total Tests: 1
+
+Test Name:   rtc_testapp_7
+
+Test Assertion
+& Strategy:  Get and set RTC time and date
+==================================================================================================*/
+
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+/*==================================================================================================
+                                        INCLUDE FILES
+==================================================================================================*/
+/* Standard Include Files */
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+    
+/* Harness Specific Include Files. */
+#include "test.h"
+#include "usctest.h"
+
+/* Verification Test Environment Include Files */
+#include "rtc_test_7.h"
+
+/*==================================================================================================
+                                        LOCAL MACROS
+==================================================================================================*/
+
+
+/*==================================================================================================
+                          LOCAL TYPEDEFS (STRUCTURES, UNIONS, ENUMS)
+==================================================================================================*/
+
+
+/*==================================================================================================
+                                       LOCAL CONSTANTS
+==================================================================================================*/
+
+/*==================================================================================================
+                                       LOCAL VARIABLES
+==================================================================================================*/
+
+
+/*==================================================================================================
+                                       GLOBAL CONSTANTS
+==================================================================================================*/
+
+
+/*==================================================================================================
+                                       GLOBAL VARIABLES
+==================================================================================================*/
+/* Extern Global Variables */
+extern int  Tst_count;               /* counter for tst_xxx routines.         */
+extern char *TESTDIR;                /* temporary dir created by tst_tmpdir() */
+
+/* Global Variables */
+char *TCID     = "rtc_testapp_7"; /* test program identifier.          */
+int  TST_TOTAL = 1;                  /* total number of tests in this file.   */
+
+/*==================================================================================================
+                                   GLOBAL FUNCTION PROTOTYPES
+==================================================================================================*/
+void cleanup(void);
+void setup(void);
+int main(int argc, char **argv);
+
+/*==================================================================================================
+                                   LOCAL FUNCTION PROTOTYPES
+==================================================================================================*/
+
+
+/*==================================================================================================
+                                       GLOBAL FUNCTIONS
+==================================================================================================*/
+
+/*================================================================================================*/
+/*===== cleanup =====*/
+/**
+@brief  Performs all one time clean up for this test on successful
+        completion,  premature exit or  failure. Closes all temporary
+        files, removes all temporary directories exits the test with
+        appropriate return code by calling tst_exit() function.cleanup
+
+@param  Input :      None.
+        Output:      None.
+  
+@return Nothing
+*/
+/*================================================================================================*/
+void cleanup(void)
+{
+        /* VTE : Actions needed to get a stable target environment */
+        int VT_rv = TFAIL;
+
+        VT_rv = VT_rtc_test7_cleanup();
+        if (VT_rv != TPASS)
+        {
+                tst_resm(TWARN, "VT_cleanup() Failed : error code = %d", VT_rv);
+        }
+
+        tst_exit();
+}
+
+/*==================================================================================================
+                                       LOCAL FUNCTIONS
+==================================================================================================*/
+
+/*================================================================================================*/
+/*===== setup =====*/
+/**
+@brief  Performs all one time setup for this test. This function is
+        typically used to capture signals, create temporary dirs
+        and temporary files that may be used in the course of this test.
+
+@param  Input :      None.
+        Output:      None.
+  
+@return On failure - Exits by calling cleanup().
+        On success - returns 0.
+*/
+/*================================================================================================*/
+void setup(void)
+{
+        int VT_rv = TFAIL;
+ 
+        VT_rv = VT_rtc_test7_setup();
+        if (VT_rv != TPASS)
+        {
+                tst_brkm(TBROK , cleanup, "VT_setup() Failed : error code = %d", VT_rv);
+        }
+    
+        return;
+}
+
+
+/*================================================================================================*/
+/*===== main =====*/
+/**
+@brief  Entry point to this test-case. It parses all the command line
+        inputs, calls the global setup and executes the test. It logs
+        the test status and results appropriately using the LTP API's
+        On successful completion or premature failure, cleanup() func
+        is called and test exits with an appropriate return code.
+
+@param  Input :      argc - number of command line parameters.
+        Output:      **argv - pointer to the array of the command line parameters.
+        Describe input arguments to this test-case
+        -l - Number of iteration
+        -v - Prints verbose output
+        -V - Prints the version number
+  
+@return On failure - Exits by calling cleanup().
+        On success - exits with 0 exit value.
+*/
+/*================================================================================================*/
+int main(int argc, char **argv)
+{
+        int VT_rv = TFAIL;
+        int t_opt = 0;
+        int o_opt = 0;
+        int seconds = 0;
+        char *msg;
+        char *alarm_time;
+        char *on_off;
+
+        /*Parse options*/
+        option_t options[]=
+        {
+            {"T:",&t_opt,&alarm_time},
+            {"O:",&o_opt,&on_off},
+            {NULL,NULL,NULL}
+        };
+        if ((msg = parse_opts(argc,argv,options,&help))!=(char*)NULL)
+        {
+            tst_resm(TFAIL,"option parsing error - %s",msg);
+            return TFAIL;
+        }
+
+        if (o_opt)
+        {
+           if ( !strcmp(on_off,"on"))
+           {
+               if (t_opt)
+               {
+                   seconds = atoi(alarm_time);
+               }
+               else
+               {
+                   help();
+                   return VT_rv;
+               }
+               setup();
+               tst_resm(TINFO, "Launching %s test", TCID);
+               VT_rv = VT_rtc_alarm_on(seconds);
+               if(VT_rv == TPASS)
+               {
+                   tst_resm(TPASS, "%s test case worked as expected", TCID);
+               }
+               else
+               {
+                   tst_resm(TFAIL, "%s test case did NOT work as expected", TCID);
+               }
+               /* cleanup allocated test ressources */
+               cleanup();
+               return VT_rv;
+           }
+           else if (!strcmp(on_off,"off"))
+           {
+               setup();
+               tst_resm(TINFO, "Launching %s test", TCID);
+               VT_rv = VT_rtc_alarm_off();
+               if(VT_rv == TPASS)
+               {
+                   tst_resm(TPASS, "%s test case worked as expected", TCID);
+               }
+               else
+               {
+                   tst_resm(TFAIL, "%s test case did NOT work as expected", TCID);
+               }
+               /* cleanup allocated test ressources */
+               cleanup();
+               return VT_rv;
+           }
+           else
+           {
+               help();
+               return VT_rv;
+           }
+        }
+        else
+        {
+            help();
+        }
+  
+        return VT_rv;	
+}
+
+/*===== help =====*/
+/** 
+@brief  Print help information testapp execution.
+
+@param  Input :      None. 
+        Output:      None. 
+
+@return None  
+*/
+/*================================================================================================*/
+void help(void)
+{
+        printf("RTC driver option\n");
+        printf("  -O on/off\t enable/disable alarm\n");
+        printf("  -T seconds\t Set RTC alarm time in second\n");
+}
---
+++ testcases/vte_tests_suite/rtc_tests/rtc_testapp_7/rtc_test_7.c
@@ -0,0 +1,310 @@
+/***
+ * **Copyright 2008-2009 Freescale Semiconductor, Inc. All Rights Reserved.
+ * **
+ * **The code contained herein is licensed under the GNU General Public
+ * **License. You may obtain a copy of the GNU General Public License
+ * **Version 2 or later at the following locations:
+ * **
+ * **http://www.opensource.org/licenses/gpl-license.html
+ * **http://www.gnu.org/copyleft/gpl.html
+ * **/
+/*================================================================================================*/
+/**
+        @file   rtc_test_7.c
+
+        @brief  RTC time and date test
+*/
+/*===================================================================================================
+Revision History:
+                            Modification     Tracking
+Author                          Date          Number    Description of Changes
+-------------------------   ------------    ----------  -------------------------------------------
+
+Blake                       12/29/2008                   Initial version
+====================================================================================================
+Portability:  ARM GCC
+==================================================================================================*/
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+/*==================================================================================================
+                                        INCLUDE FILES
+==================================================================================================*/
+/* Standard Include Files */
+#include <errno.h>
+    
+/* Harness Specific Include Files. */
+#include "test.h"
+
+/* Verification Test Environment Include Files */
+#include "rtc_test_7.h"
+
+/*==================================================================================================
+                                        LOCAL MACROS
+==================================================================================================*/
+#if !defined(TRUE) && !defined(FALSE)
+        #define TRUE  1
+        #define FALSE 0
+#endif  
+#define PRECISION 2
+
+/*==================================================================================================
+                          LOCAL TYPEDEFS (STRUCTURES, UNIONS, ENUMS)
+==================================================================================================*/
+enum RTC_STATE
+{
+        RTC_TIME = 0,     /* Current time */	
+        RTC_ALARM_TIME,   /* Current time of alarm */
+        RTC_WKALARM_TIME, /* Current time of wake up alarm */
+        RTC_TOTAL_STATES  /* Total count of the states */
+};
+
+/*==================================================================================================
+                                       LOCAL CONSTANTS
+==================================================================================================*/
+
+
+/*==================================================================================================
+                                       LOCAL VARIABLES
+==================================================================================================*/
+int               file_desc = -1; 
+struct rtc_time   saved_time;    /* Saved time value */	
+struct rtc_time   saved_alarm;   /* Saved alarms time value */
+struct rtc_wkalrm saved_wkalarm; /* Saved wake up alarms time value */
+int               saved_states[RTC_TOTAL_STATES] = {0}; /* Flags */
+int               is_ok = 1;			
+
+/*==================================================================================================
+                                       GLOBAL CONSTANTS
+==================================================================================================*/
+
+
+/*==================================================================================================
+                                       GLOBAL VARIABLES
+==================================================================================================*/
+
+
+/*==================================================================================================
+                                   LOCAL FUNCTION PROTOTYPES
+==================================================================================================*/
+int ask_user(char *question);
+
+/*===== ask_user =====*/
+/**
+@brief  Asks user to answer the question
+
+@param  Input:  char * question
+        Output: None
+  
+@return TPASS - if user asks "Y", yes
+        TFAIL - if user asks "N", no
+*/
+int ask_user(char *question)
+{
+        unsigned char answer;
+        int           ret = TRETR;
+
+        do
+        {
+                tst_resm(TINFO, "%s [Y/N]", question);
+                answer = fgetc(stdin);
+                if (answer == 'Y' || answer == 'y')
+                        ret = TPASS;
+                else if (answer == 'N' || answer == 'n')
+                ret = TFAIL;
+        }
+        while (ret == TRETR);
+
+        // Wipe CR character from stream 
+        fgetc(stdin);      
+        return ret;
+}
+
+
+/*================================================================================================*/
+/*===== VT_rtc_test6_setup =====*/
+/**
+@brief  assumes the pre-condition of the test case execution
+
+@param  None
+  
+@return On success - return TPASS
+        On failure - return the error code
+*/
+/*================================================================================================*/
+int VT_rtc_test7_setup(void)
+{
+        int rv = TFAIL;
+
+        file_desc = open (RTC_DRIVER_NAME, O_RDONLY);
+        if (file_desc ==  -1)
+        {
+                tst_brkm(TBROK, cleanup, "ERROR : Open RTC driver fails");
+                perror("cannot open /dev/misc/rtc");
+        }
+        else
+        {
+                rv = TPASS;
+        }
+    
+        return rv;
+}
+
+
+/*================================================================================================*/
+/*===== VT_rtc_test7_cleanup =====*/
+/**
+@brief  assumes the post-condition of the test case execution
+
+@param  None
+  
+@return On success - return TPASS
+        On failure - return the error code
+*/
+/*================================================================================================*/
+int VT_rtc_test7_cleanup(void)
+{
+        int rv = TFAIL;
+        int ret = 0;
+        int i;
+
+        /* close RTC driver file descriptor */
+        ret = close(file_desc);
+
+        /* Close returns -1 in case of failure */
+        if (ret == -1)
+        {
+                tst_resm(TWARN, "ERROR : Close RTC driver fails");
+                perror("cannot close /dev/misc/rtc");
+        }
+        else
+        {
+                rv = TPASS;
+        }
+
+        return rv;
+}
+
+
+/*================================================================================================*/
+/*===== VT_rtc_test7 =====*/
+/**
+@brief  - Read and set time and date
+        - Set alarm and read alarm settings
+        - Enable alarm. Wait for alarm interrupt. Disable alarm
+
+@param  int seconds - alarm time in second
+  
+@return On success - return TPASS
+        On failure - return the error code
+*/
+/*================================================================================================*/
+int VT_rtc_alarm_on(int seconds)
+{
+        int rv = TFAIL;
+        int retval, irqcount = 0;
+        unsigned long data = 0;
+
+        struct rtc_time rtc_tm = {0,0,0,0,0,0};
+
+        tst_resm( TINFO, "RTC Driver Test 7" );
+        tst_resm( TINFO, "RTC date, time and alarm" );
+
+
+
+        /***************************************/
+        /* Test for alarm                      */
+        /***************************************/
+        tst_resm( TINFO, "" );
+        tst_resm( TINFO, "ALARM TEST: RTC_ALM_SET & RTC_ALM_READ" );
+
+        /* Read the RTC time/date */
+        tst_resm( TINFO, "  Read date/time..." );
+        retval = ioctl( file_desc, RTC_RD_TIME, &rtc_tm );
+        if( retval < 0 )
+        {
+                tst_resm(TFAIL, "    ioctl RTC_RD_TIME fails: %s \n", strerror(errno));
+                return TFAIL;
+        }
+
+        tst_resm( TINFO, "    Current RTC date/time is %d-%d-%d, %02d:%02d:%02d.",
+                rtc_tm.tm_mday, rtc_tm.tm_mon+1, rtc_tm.tm_year+1900,
+                rtc_tm.tm_hour, rtc_tm.tm_min, rtc_tm.tm_sec );
+
+        /* Set the alarm to 5 sec in the future, and check for rollover */
+        tst_resm(TINFO, "  Set the alarm to %d seconds in the future..." ,seconds);
+        rtc_tm.tm_sec += seconds;
+        if (rtc_tm.tm_sec >= 60)
+        {
+                rtc_tm.tm_min += rtc_tm.tm_sec/60;
+                rtc_tm.tm_sec %= 60;
+        }
+        if  (rtc_tm.tm_min >= 60)
+        {
+                rtc_tm.tm_hour += rtc_tm.tm_min/60;
+                rtc_tm.tm_min %= 60;
+        }
+        if  (rtc_tm.tm_hour >= 24)
+        {
+                tst_brkm( TRETR, cleanup, "    Run test issue. Restart it." );
+        }
+
+        retval = ioctl( file_desc, RTC_ALM_SET, &rtc_tm );
+        if( retval < 0 )
+        {
+                tst_resm(TFAIL, "    ioctl RTC_ALM_SET fails: %s \n", strerror(errno));
+                is_ok = 0;
+        }
+
+        /* Enable alarm interrupts */
+        tst_resm(TINFO, "    Enable alarm interrupts" );
+        retval = ioctl( file_desc, RTC_AIE_ON, 0 );
+        if( retval < 0 )
+        {
+                tst_resm(TFAIL, "    ioctl RTC_AIE_ON fails: %s \n", strerror(errno));
+                return TFAIL;
+        }
+
+        tst_resm(TINFO, "  Waiting %d seconds for alarm......." ,seconds);
+         
+    
+        /* Disable alarm interrupts */
+        tst_resm(TINFO, "  Disable alarm interrupts" );
+        retval = ioctl( file_desc, RTC_AIE_OFF, 0 );
+        if( retval < 0 )
+        {
+                tst_resm(TFAIL, "    ioctl RTC_AIE_OFF fails: %s \n", strerror(errno));
+                return TFAIL;
+        }
+
+        /******** end of test for alarm *********/
+
+       
+        /***************************************/
+        /* Final test result                   */
+        /***************************************/
+        
+        rv = is_ok!=0?TPASS:TFAIL;
+        return rv;
+}
+
+int  VT_rtc_alarm_off(void)
+{
+    int retval;
+
+    /* Disable alarm interrupts */
+    tst_resm(TINFO, "  Disable alarm interrupts" );
+    retval = ioctl( file_desc, RTC_AIE_OFF, 0 );
+    if( retval < 0 )
+    {
+        tst_resm(TFAIL, "    ioctl RTC_AIE_OFF fails: %s \n", strerror(errno));
+        return TFAIL;
+    }
+    return TPASS;
+}
+
+#ifdef __cplusplus
+}
+#endif
