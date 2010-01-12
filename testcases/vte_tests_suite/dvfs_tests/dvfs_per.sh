b8adfdf685d261e690a646c1dafb4afffbdddfcf
Spring Zhang <spring.zhang@freescale.com>
2010-01-12 16:15:18 +0800

    ltp-vte DVFS-PER-add DVFS-PER test scripts 1. Add basic test 2. Add bonnie stress test
    Signed-off-by: Spring Zhang <spring.zhang@freescale.com>

---
+++ testcases/vte_tests_suite/dvfs_tests/dvfs_per.sh
@@ -0,0 +1,261 @@
+#!/bin/sh
+#Copyright 2010 Freescale Semiconductor, Inc. All Rights Reserved.
+#
+#This program is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 2 of the License, or
+#(at your option) any later version.
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License along
+#with this program; if not, write to the Free Software Foundation, Inc.,
+#51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+##############################################################################
+#
+#
+# File :       dvfs_per.sh
+#
+# Description: Test DVFS peripheral function and stress test
+#    
+#==============================================================================
+#Revision History:
+#                       Modification     Tracking
+# Author                    Date          Number    Description of Changes
+#--------------------   ------------    ----------  ----------------------
+# Spring Zhang           Jan.12,2010         n/a      Initial version
+
+# Function:     usage
+#
+# Description:  - display help info.
+#
+# Return        - none
+usage()
+{
+    cat <<-EOF
+
+    Use this command to test DVFS PER functions.
+    usage: ./${0##*/} 1  -- DVFS PER basic test
+           ./${0##*/} 2  -- DVFS PER stress test
+           e.g.: ./${0##*/} 2
+
+EOF
+}
+
+# Function:     setup
+#        
+# Description:  - Check if required commands exits
+#               - Export global variables
+#               - Check if required config files exits
+#               - Create temporary files and directories
+#   
+# Return        - zero on success
+#               - non zero on failure. return value from commands ($RC)
+setup()
+{
+    RC=0
+
+    # Total number of test cases in this file. 
+    export TST_TOTAL=2
+
+    # The TCID and TST_COUNT variables are required by the LTP 
+    # command line harness APIs, these variables are not local to this program.
+
+    # Test case identifier
+    export TCID="TGE_LV_DVFS_PER"
+    # Set up is initialized as test 0
+    export TST_COUNT=0
+    # Initialize cleanup function to execute on program exit.
+    # This function will be called before the test program exits.
+    BIN_DIR=`dirname $0`
+    export PATH=$PATH:$BIN_DIR
+
+    LTPTMP=${TMP}        # Temporary directory to create files, etc.
+    if [ -z $LTPTMP ]; then
+        LTPTMP=/tmp
+    fi
+
+    trap "cleanup" 0
+
+    return $RC
+}
+
+cleanup()
+{
+    echo 0 > $DVFS_PER_DIR/enable
+    echo 0 > /sys/bus/platform/drivers/mxc_dvfs_core/mxc_dvfs_core.0/enable
+}
+
+# analyze the result
+anl_res()
+{
+    RC=$1
+    if [ $RC -ne 0 ]; then
+        tst_resm TFAIL "Fail in DVFS-PER test: $2"
+        exit $RC
+    fi
+}
+
+dvfs_per_basic()
+{
+    export TST_COUNT=1
+
+    RC=0
+
+    platfm.sh || platfm=$?
+    if [ $platfm -eq 67 ]; then
+        RC=$platfm
+        return $RC
+    fi
+
+    DVFS_PER_DIR=/sys/devices/platform/mxc_dvfsper.0
+    if [ $platfm -eq 51 ] || [ $platfm -eq 41 ]; then
+        echo 1 > $DVFS_PER_DIR/enable
+    else
+        tst_resm TWARN "platform not support"
+        RC=67
+        return $RC
+    fi
+
+    res=`cat $DVFS_PER_DIR/enable | grep "enabled" | wc -l`
+    if [ $res -eq 1 ]; then
+        tst_resm TPASS "DVFS-PER is enabled"
+    else
+        tst_resm TFAIL "fail to enable DVFS-PER"
+        RC=1
+        return $RC
+    fi
+
+    #LCD test
+    lcd_testapp -T 1 -B /dev/fb0 -D 16 -X 10
+    anl_res $? LCD
+
+    #storage test
+    storage_test.sh CD /mnt/mmcblk0p1 /mnt/msc 100 3
+    anl_res $? STORAGE
+
+    #V4L2 test
+    v4l_capture_testapp -C 3 -O BGR32 -o ./output
+    anl_res $? V4L2
+
+    #suspend test
+    sleep 3 
+    echo "*************************************"
+    echo "* please press key to resume system *"
+    echo "*************************************"
+    echo -n standby > /sys/power/state
+    tst_resm TPASS "Resume from suspend..."
+
+    #enable dvfs-core
+    echo 1 > /sys/bus/platform/drivers/mxc_dvfs_core/mxc_dvfs_core.0/enable
+    res=`cat /sys/bus/platform/drivers/mxc_dvfs_core/mxc_dvfs_core.0/enable | grep "enabled" | wc -l`
+    if [ $res -ne 1 ]; then
+        tst_resm TFAIL "Fail to enable dvfs-core"
+        RC=1
+        return $RC
+    fi
+
+    #ALSA capture test
+    adc_test1.sh -f S16_LE -d 5 -c 1 -r 44100 -A
+    anl_res $? ALSA
+
+    sleep 5
+    echo "*************************************"
+    echo "* please press key to resume system *"
+    echo "*************************************"
+    echo -n mem > /sys/power/state
+    tst_resm TPASS "Resume from mem..."
+
+    res=`cat /proc/cpu/clocks | grep ahb | grep 33250000 | wc -l`
+    if [ $res -eq 2 ]; then
+        tst_resm TPASS "DVFS-PER basic function test"
+    fi
+    
+    echo 0 > $DVFS_PER_DIR/enable
+    res=`cat $DVFS_PER_DIR/enable | grep "disabled" | wc -l`
+    if [ $res -eq 1 ]; then
+        tst_resm TPASS "DVFS-PER is disabled"
+    else
+        tst_resm TFAIL "fail to enable DVFS-PER"
+        RC=1
+        return $RC
+    fi
+
+    return $RC
+}
+
+# ToDo: add suspend resume stress test
+dvfs_per_stress()
+{
+    export TST_COUNT=2
+    export TCID="TGE_LV_DVFS_PER_STRESS"
+
+    RC=0
+
+    platfm.sh || platfm=$?
+    if [ $platfm -eq 67 ]; then
+        RC=$platfm
+        return $RC
+    fi
+
+    DVFS_PER_DIR=/sys/devices/platform/mxc_dvfsper.0
+    if [ $platfm -eq 51 ] || [ $platfm -eq 41 ]; then
+        echo 1 > $DVFS_PER_DIR/enable
+    else
+        tst_resm TWARN "platform not support"
+        RC=67
+        return $RC
+    fi
+
+    res=`cat $DVFS_PER_DIR/enable | grep "enabled" | wc -l`
+    if [ $res -eq 1 ]; then
+        tst_resm TPASS "DVFS-PER is enabled"
+    else
+        tst_resm TFAIL "fail to enable DVFS-PER"
+        RC=1
+        return $RC
+    fi
+
+    mkdir -p /mnt/msc && mount -t vfat /dev/sda1 /mnt/msc
+    df | grep msc
+    anl_res $? MOUNT_MSC
+
+    tst_resm TINFO "Start bonnie++ stress test"
+    i=0
+    while [ $i -lt 100 ]; do
+        bonnie++ -d /mnt/msc -s 32 -r 16 -u 0:0 -m FSL
+        anl_res $? bonnie_test
+        tst_resm TINFO "bonnie++ times: $i"
+        i=`expr i + 1`
+    done
+
+    tst_resm TPASS "Pass bonnie++ stress test"
+}
+
+# Function:     main
+#
+# Description:  - Execute all tests, exit with test status.
+#
+# Exit:         - zero on success
+#               - non-zero on failure.
+#
+# Return value from setup, and test functions.
+RC=0
+
+# bash specified script, using array, not dash-compatibility
+setup  || exit $RC
+
+case "$1" in
+    1)
+    dvfs_per_basic || exit $RC
+    ;;
+    2)
+    dvfs_per_stress || exit $RC
+    ;;
+    *)
+    usage
+    exit 67
+    ;;
+esac
+
