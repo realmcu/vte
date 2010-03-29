234f635861a74b876acb4a389b9a22fadddabe5e
Hake Huang <b20222@freescale.com>
2010-03-29 16:37:37 +0800

    ltp-vte: add stop usb scripts
    Signed-off-by: Hake Huang <b20222@freescale.com>

---
+++ testcases/vte_tests_suite/power_manager/usb_pm/stop-usb.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+# #The code contained herein is licensed under the GNU General Public
+#License. You may obtain a copy of the GNU General Public License
+#Version 2 or later at the following locations:
+#
+#http://www.opensource.org/licenses/gpl-license.html
+#http://www.gnu.org/copyleft/gpl.html
+#
+#pre-requiste: better to remove all usb devices before run the testcases
+#
+
+FSL_EHCI_INTERFACE="fsl-ehci.*"
+FSL_USB2_UDC="fsl-usb2-udc"
+
+SKIP_USBPORT="1.6"
+
+SYS_USB_DEV="/sys/bus/usb/devices/*"
+
+enable_usb_wakeup()
+{
+ usb_ehci_ctrls=$(ls /sys/devices/platform/${FSL_EHCI_INTERFACE}/power/wakeup)
+ usb_ctrls=$(ls /sys/devices/platform/${FSL_USB2_UDC}/power/wakeup)" "${usb_ehci_ctrls}
+ for i in $usb_ctrls
+ do
+    echo enabled > $i
+    echo "echo enabled > $i"
+ done
+}
+
+suspend_usb_dev()
+{
+ prd_list=$(ls ${SYS_USB_DEV}/product)
+ for i in $prd_list
+ do
+   is_str=$(cat $i | grep Storage)
+   if [ ! -z "$is_str" ]; then
+      echo suspend > $(echo $i | sed 's/product/power/')/level
+      echo "echo suspend > $(echo $i | sed 's/product/power/')/level"
+   fi
+ done
+}
+
+auto_usb_dev()
+{
+ prd_list=$(ls ${SYS_USB_DEV}/power/level)
+ for i in $prd_list
+ do
+      is_skip=$(echo $i | grep $SKIP_USBPORT)
+      if [ -z "$is_skip" ]; then
+      echo auto > $i
+      echo "echo auto > $i"
+      fi
+  done
+}
+
+
+#check if root
+is_root=$(whoami)
+if [ $is_root != "root" ];then
+echo "not root!"
+exit 255
+fi
+
+enable_usb_wakeup
+suspend_usb_dev
+auto_usb_dev
+
+echo "please unplug all usb devices in 15 seconds"
+
+sleep 15
+
+#usb_cnt=$(lsusb | wc -l)
+#while [ $usb_cnt -gt 4 ]
+#do
+#echo "please unplug all usb devices"
+#sleep 5
+#usb_cnt=$(lsusb | wc -l)
+#done
+
+usb_clk_list=$(cat /proc/cpu/clocks | grep usb | cut -c 51-100 | awk {'print $1'})
+
+RT=0
+for i in $usb_clk_list
+do
+  if [ $i -ne 0 ]; then
+     echo "usb clock is not zero"
+     RT=1
+  fi
+done
+exit $RT
