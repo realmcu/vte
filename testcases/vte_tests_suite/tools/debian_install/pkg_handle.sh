109631e42d87b59eb54a6005cd94e71f710b4348
Hake Huang <b20222@freescale.com>
2010-03-31 16:37:17 +0800

    ltp-vte: add tools for install/remove debian package
    Signed-off-by: Hake Huang <b20222@freescale.com>

---
+++ testcases/vte_tests_suite/tools/debian_install/pkg_handle.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -x
+# $1 install / remove
+# $2 path to debian package folder
+
+helpme()
+{
+ echo "$1 [install/remove] [path to debian package]"
+}
+
+
+
+install_debian_package()
+{
+ list=$(ls $1/*.deb)
+ for i in $list
+ do
+ dpkg -i $1/$i
+ done
+ return 0
+}
+
+remove_debian_package()
+{
+ list=$(ls $1/*.deb)
+ for i in $list
+ do
+ dpkg -r $(echo $i | cut -f 1 -d _)
+ done
+ return 0
+}
+
+if [ $# -ne 2 ];then
+  helpme
+  exit 1
+fi
+
+if [ $1 = "install" ];then
+  echo $2
+  install_debian_package $2 || exit 1
+  exit 0
+fi
+
+if [ $1 = "remove" ]; then
+  remove_debian_package $2 || exit 1
+  exit 0
+fi
+
+
