06ec07df8825209c8a0e52aefcdcdb4efad05f52
Hake Huang <b20222@freescale.com>
2010-07-16 14:34:43 +0800

    ltp-vte: enable the draw pic for epxp test
    Signed-off-by: Hake Huang <b20222@freescale.com>

--- testcases/vte_tests_suite/epxp_test/lib/pxp_lib.h
+++ testcases/vte_tests_suite/epxp_test/lib/pxp_lib.h
@@ -2,7 +2,9 @@
  * pxp_lib - a user space library for PxP
  *
  * Copyright (C) 2010 Freescale Semiconductor, Inc.
- *
+ */
+
+/*
  * The code contained herein is licensed under the GNU Lesser General
  * Public License.  You may obtain a copy of the GNU Lesser General
  * Public License Version 2.1 or later at the following locations:
@@ -35,10 +37,6 @@ void pxp_release_channel(pxp_chan_handle_t *pxp_chan);
 int pxp_config_channel(pxp_chan_handle_t *pxp_chan, struct pxp_config_data *pxp_conf);
 int pxp_start_channel(pxp_chan_handle_t *pxp_chan);
 int pxp_wait_for_completion(pxp_chan_handle_t *pxp_chan, int times);
-int pxp_get_phymem(struct pxp_mem_desc *mem);
-int pxp_put_phymem(struct pxp_mem_desc *mem);
-int pxp_get_virtmem(struct pxp_mem_desc *mem);
-int pxp_put_virtmem(struct pxp_mem_desc *mem);
 int pxp_get_mem(struct pxp_mem_desc *mem);
 int pxp_put_mem(struct pxp_mem_desc *mem);
 
