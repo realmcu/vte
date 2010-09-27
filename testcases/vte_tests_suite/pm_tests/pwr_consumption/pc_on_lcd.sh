438f682b6645704b843518778fde91daad494bce
Spring Zhang <spring.zhang@freescale.com>
2010-09-27 17:27:48 +0800

    ltp-vte PowerConsumption-no need to operate fb2 for it is TVout
    Signed-off-by: Spring Zhang <spring.zhang@freescale.com>

--- testcases/vte_tests_suite/pm_tests/pwr_consumption/pc_on_lcd.sh
+++ testcases/vte_tests_suite/pm_tests/pwr_consumption/pc_on_lcd.sh
@@ -18,5 +18,6 @@
 ###############################################################################
 echo 0 > /sys/class/graphics/fb0/blank
 echo 0 > /sys/class/graphics/fb1/blank
-echo 0 > /sys/class/graphics/fb2/blank
+#delete it for fb2 is TVout
+#echo 0 > /sys/class/graphics/fb2/blank
 
