db3d2438ff04e4626decf6632dfb4c3627748c54
Hake Huang <b20222@freescale.com>
2010-10-27 14:23:23 +0800

    ltp-vte: rtc_4 process signal 11 to avoid abort
    Signed-off-by: Hake Huang <b20222@freescale.com>

--- testcases/vte_tests_suite/rtc_tests/rtc_testapp_4/rtc_test_4.c
+++ testcases/vte_tests_suite/rtc_tests/rtc_testapp_4/rtc_test_4.c
@@ -1,5 +1,5 @@
 /***
-**Copyright (C) 2004-2009 Freescale Semiconductor, Inc. All Rights Reserved.
+**Copyright (C) 2004-2010 Freescale Semiconductor, Inc. All Rights Reserved.
 **
 **The code contained herein is licensed under the GNU General Public
 **License. You may obtain a copy of the GNU General Public License
@@ -38,8 +38,19 @@ extern "C"{
                                         INCLUDE FILES
 ==================================================================================================*/
 /* Standard Include Files */
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <errno.h>
-    
+#include <string.h>
+#include <signal.h>
+#include <time.h>
+#include <execinfo.h>
+#include <limits.h>
+#include <ucontext.h>
+#include <sys/types.h>
+#include <setjmp.h>    
 /* Harness Specific Include Files. */
 #include "test.h"
 
@@ -63,13 +74,14 @@ extern "C"{
                                        LOCAL CONSTANTS
 ==================================================================================================*/
 #define BAD_RTC_IOCTL 0x20
+#define arrsizeof(x) (sizeof(x)/sizeof(x[0]))
 
 /*==================================================================================================
                                        LOCAL VARIABLES
 ==================================================================================================*/
 int file_desc = -1;
 int is_ok = TRUE;
-
+jmp_buf   jmpbuf;
 /*==================================================================================================
                                        GLOBAL CONSTANTS
 ==================================================================================================*/
@@ -173,6 +185,119 @@ int VT_rtc_test4_cleanup(void)
         return rv;
 }
 
+//
+//
+//
+char *getProcessName (void)
+{
+  char buffer [128];
+  FILE *fp;
+
+  sprintf (buffer, "/proc/%d/stat", getpid ());
+
+  if ((fp = fopen (buffer, "r")))
+  {
+    if (fgets (buffer, sizeof (buffer), fp))
+    {
+      char *s;
+
+      if ((s = index (buffer, ')')))
+      {
+        *s = '\0';
+
+        if ((s = index (buffer, '(')))
+          return ++s;
+      }
+    }
+  }
+
+  return NULL;
+}
+
+//
+//
+//
+static void sighandlerPrint (FILE *fp, int signo, int code, ucontext_t *context, void *bt [], int bt_size)
+{
+  char *processName;
+  time_t ttime = time (NULL);
+
+#ifdef __arm__
+  fprintf (fp, "%s", ctime (&ttime));
+  fprintf (fp, "PID=%d (%s)\n", getpid (), (processName = getProcessName ()) ? processName : "unknown");
+  fprintf (fp, "signo=%d/%s\n", signo, strsignal (signo));
+  fprintf (fp, "code=%d (not always applicable)\n", code);
+  fprintf (fp, "\nContext: 0x%08lx\n", (unsigned long) context);
+  fprintf (fp, "    trap_no: 0x%08lx   error_code: 0x%08lx   oldmask: 0x%08lx\n"
+               "   arm_r0: 0x%08lx  arm_r1: 0x%08lx  arm_r2: 0x%08lx  arm_r3: 0x%08lx\n"
+               "   arm_r4: 0x%08lx  arm_r5: 0x%08lx  arm_r6: 0x%08lx  arm_r7: 0x%08lx\n"
+               "  arm_r8:  0x%8lx  arm_r9: 0x%08lx  arm_r10: 0x%08lx   arm_fp: 0x%08lx\n"
+               "  arm_ip: 0x%08lx   arm_sp: 0x%08lx   arm_lr: 0x%08lx  arm_pc: 0x%08lx\n"
+               "  arm_cpsr: 0x%08lx   fault_address: 0x%08lx\n",
+               context->uc_mcontext.trap_no,
+							 context->uc_mcontext.error_code,
+							 context->uc_mcontext.oldmask,
+							 context->uc_mcontext.arm_r0,
+               context->uc_mcontext.arm_r1,
+							 context->uc_mcontext.arm_r2,
+							 context->uc_mcontext.arm_r3,
+							 context->uc_mcontext.arm_r4,
+               context->uc_mcontext.arm_r5,  
+							 context->uc_mcontext.arm_r6,
+							 context->uc_mcontext.arm_r7,
+							 context->uc_mcontext.arm_r8,
+               context->uc_mcontext.arm_r9,
+							 context->uc_mcontext.arm_r10,
+							 context->uc_mcontext.arm_fp,
+							 context->uc_mcontext.arm_ip,
+               context->uc_mcontext.arm_sp,  
+							 context->uc_mcontext.arm_lr,
+							 context->uc_mcontext.arm_pc,
+							 context->uc_mcontext.arm_cpsr,
+							 context->uc_mcontext.fault_address
+  );
+
+  fprintf (fp, "\n%d elements in backtrace\n", bt_size);
+  fflush (fp);
+#endif
+  backtrace_symbols_fd (bt, bt_size, fileno (fp));
+}
+
+
+
+static void sighandlerABRT (int signo, struct siginfo *si, void *ctx)
+{
+  void *bt [128];
+  int bt_size;
+
+  bt_size = backtrace (bt, arrsizeof (bt));
+
+  sighandlerPrint (stderr, signo, si->si_code, (ucontext_t *) ctx, bt, bt_size);
+
+  longjmp(jmpbuf,1);
+  /*exit (1);*/
+}
+
+
+int installSignalHandlers (void)
+{
+  struct sigaction sa;
+
+  sigemptyset (&sa.sa_mask);
+  sigaddset (&sa.sa_mask, SIGABRT);
+
+  sa.sa_flags = SA_ONESHOT | SA_SIGINFO;
+  sa.sa_sigaction = sighandlerABRT;
+
+  if (sigaction (SIGABRT, &sa, NULL))
+  {
+    fprintf (stderr, "sigaction failed, line %d, %d/%s\n", __LINE__, errno, strerror (errno));
+    exit (1);
+  }
+  printf("abort signal installed\n");
+  return 1;
+}
+
 
 /*================================================================================================*/
 /*===== VT_rtc_test4 =====*/
@@ -197,10 +322,11 @@ int VT_rtc_test4(void)
         struct rtc_time rtc_tm;
 //        struct rtc_wkalrm wakeup_alarm;
         int is_passed = FALSE;
-        
-        tst_resm(TINFO, "RTC Driver error cases test");
-        
+				int result;
 
+        tst_resm(TINFO, "RTC Driver error cases test2");
+
+ 				installSignalHandlers ();
         /**************************/
         /* Do a non-blocking read */
         /**************************/
@@ -229,22 +355,29 @@ int VT_rtc_test4(void)
         /*******************************************/
         is_passed = 0;
         tst_resm(TINFO, "Pass negative size of buffer in read()" );
-        
-        buffer_size = -2;
-        retval = read( file_desc, &data, buffer_size );
-        if( retval < 0 )
-        {                
+        result = setjmp(jmpbuf);
+
+				if(result == 0)
+				{
+					buffer_size = -2;
+					retval = read( file_desc, &data, buffer_size );
+          if( retval < 0 )
+        	{                
                 is_passed = TRUE;
                 tst_resm( TINFO, "Errno: %d, Reason: %s", errno, strerror(errno) );
-        }
-        if( is_passed )
-        {
+        	}
+        	if( is_passed )
+        	{
                 tst_resm( TPASS, "It's working as expected\n" );
-        }
-        else
-        {
+        	}
+        	else
+        	{
                 tst_resm( TFAIL, "It's not working as expected\n" );
-        }
+        	}
+				}else{
+                is_passed = TRUE;
+                tst_resm( TPASS, "It's working as expected\n" );
+				}
         is_ok = is_ok && is_passed;
         
         
