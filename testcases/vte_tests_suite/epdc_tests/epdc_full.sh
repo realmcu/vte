10bfd8e3a75dbb407063b13f9d68a17be0eef5be
Hake Huang <b20222@shlx12.ap.freescale.net>
2010-11-24 01:44:14 +0800

    ltp-vte: epdc add flags support to update mode
    Signed-off-by: Hake Huang <b20222@shlx12.ap.freescale.net>

--- testcases/vte_tests_suite/epdc_tests/epdc_full.sh
+++ testcases/vte_tests_suite/epdc_tests/epdc_full.sh
@@ -82,6 +82,8 @@ tst_resm TINFO "test $TST_COUNT: $TCID "
 LOOP_TIMES=2
 ROT_LIST="0 1 2 3"
 CASE_LIST="2 3 4 5 6 7"
+EPDC_FLAGS="0x01 0x02"
+#alt buffer is not test here
 
 while [ $LOOP_TIMES -gt 0 ]
 do
@@ -90,8 +92,11 @@ do
 		for j in $ROT_LIST
 		do
 			echo "epdc_test -T $i -R $j"
-			epdc_test -T $i -R $j || RC=$(expr $RC + 1)
+			for k in $EPDC_FLAGS
+			do
+			epdc_test -T $i -R $j -s 0:0:128:128,257,0,0,$k,0,0,0:0:0:0 || RC=$(expr $RC + 1)
 			sleep 1
+			done
 		done
 	done
 	LOOP_TIMES=$(expr $LOOP_TIMES - 1)
