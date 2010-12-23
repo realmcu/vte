5aa7375293a1b6d201437576621c60297d5b8b53
Hake Huang <b20222@shlx12.ap.freescale.net>
2010-12-23 17:50:18 +0800

    ltp-vte: take task swithing time into account, and enable -DHOST_BUILD for local build
    Signed-off-by: Hake Huang <b20222@shlx12.ap.freescale.net>

--- testcases/vte_tests_suite/timer_test/timer_resolution.c
+++ testcases/vte_tests_suite/timer_test/timer_resolution.c
@@ -29,14 +29,21 @@
 #include <sys/time.h>
 #include <signal.h>
 #include <stdlib.h>
-#include <test.h>
-#include <usctest.h>
 #include <sys/syscall.h>
 #include <unistd.h>
 #include <time.h>
 #include <errno.h>
 
 #define gettimeofday(a,b)  syscall(__NR_gettimeofday,a,b)
+#define MAX_LOOP 100
+
+#ifdef HOST_BUILD
+#define tst_resm(a,b,...) printf(b,##__VA_ARGS__); printf("\n")
+#define tst_brkm(a,b,c,...) printf(c,##__VA_ARGS__)
+#else
+#include <test.h>
+#include <usctest.h>
+#endif
 
 char *TCID = "timer resolutions";	/* Test program identifier.    */
 int TST_TOTAL = 1;		/* Total number of test cases. */
@@ -49,10 +56,11 @@ char *lres = "10";
 
 sig_atomic_t done;
 
+#ifndef HOST_BUILD
 option_t opts[] = { {"T:", &Tflag, &tlen}, 
                     {"L:", &Lflag, &lres},
 										{} };
-
+#endif
 void alarm_handle(int sig)
 {
 	done = 1;
@@ -60,9 +68,10 @@ void alarm_handle(int sig)
 
 void cleanup(void)
 {
+#ifndef HOST_BUILD
 	TEST_CLEANUP;
-
 	tst_exit();
+#endif
 }
 
 void help()
@@ -72,16 +81,19 @@ void help()
 
 int main(int ac, char **av)
 {
+	int delay=0;
+	int iloop = MAX_LOOP;
 	struct timeval tv1, tv2;
 	char *msg;
 
+#ifndef HOST_BUILD
 	if ((msg = parse_opts(ac, av, opts, help)) != (char *)NULL) {
 		tst_brkm(TBROK, cleanup, "OPTION PARSING ERROR - %s", msg);
 	}
 
 	tst_sig(NOFORK, DEF_HANDLER, cleanup);
 	TEST_PAUSE;
-
+#endif
 	tst_resm(TINFO, "checking if gettimeofday resolution, takes %ss",
 		 tlen);
 	signal(SIGALRM, alarm_handle);
@@ -105,26 +117,40 @@ int main(int ac, char **av)
 			cleanup();
 			return 1;
 		}
-		tv1 = tv2;
-		/*now try to get the minimum time interval*/
-		usleep(1);//sleep 1us
+		/*start measue the task transfer time*/
+		iloop = MAX_LOOP;
+		while(iloop--){
+			int cd = 0;
+			if (gettimeofday(&tv2, NULL) != 0)
+			tst_brkm(TBROK, cleanup,"loop gettimeofday() failed: %s\n",strerror(errno));
+			usleep(1);
+			if (gettimeofday(&tv1, NULL) != 0)
+			tst_brkm(TBROK, cleanup,"loop gettimeofday() failed: %s\n",strerror(errno));
+      cd = 1000000 * (tv1.tv_sec - tv2.tv_sec) + (tv1.tv_usec - tv2.tv_usec) - 1;
+			delay = delay > cd ? delay : cd;
+		}
+		tst_resm(TINFO,"average task transfer delay is %d",delay);
 		if (gettimeofday(&tv2, NULL) != 0)
 			tst_brkm(TBROK, cleanup,
 				 "loop gettimeofday() failed: %s\n",
 				 strerror(errno));
-		if (1000000 * (tv2.tv_sec - tv1.tv_sec)
-		    + (tv2.tv_usec - tv1.tv_usec) > atoi(lres)) {
+		/*now try to get the minimum time interval*/
+	  usleep(1);	
+		if (gettimeofday(&tv1, NULL) != 0)
+			tst_brkm(TBROK, cleanup,
+				 "loop gettimeofday() failed: %s\n",
+				 strerror(errno));
+		if (1000000 * (tv1.tv_sec - tv2.tv_sec)
+		    + (tv1.tv_usec - tv2.tv_usec) - 2 * delay > atoi(lres)) {
 			tst_resm(TFAIL,
 				 "Time resolution is not achieved %d old %jd.%jd vs new %jd.%jd",atoi(lres),
-				 (intmax_t)tv1.tv_sec, (intmax_t)tv1.tv_usec, (intmax_t)tv2.tv_sec,
-				 (intmax_t)tv2.tv_usec);
+				 (intmax_t)tv2.tv_sec, (intmax_t)tv2.tv_usec, (intmax_t)tv1.tv_sec,
+				 (intmax_t)tv1.tv_usec);
 			cleanup();
 			return 1;
 		}
-	}
-
 	tst_resm(TPASS, "gettimeofday resolution is less than %s", lres);
-
+ }
 	cleanup();
 	return 0;
 }
