6dfc446c01e303c8973c0f230fe41a28a50c8e2d
Hake Huang <b20222@freescale.com>
2011-05-19 16:55:43 +0800

    add c-ray full test script
    Signed-off-by: Hake Huang <b20222@freescale.com>

---
+++ testcases/third_party_suite/c-ray/c-ray.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+BASE=$(dirname $0)
+#!/bin/sh
+cat ${BASE}/scene | c-ray-f > /dev/null
+cat ${BASE}/scene | c-ray-mt -t 32 > /dev/null
+cat ${BASE}/sphfract | c-ray-f > /dev/null
+cat ${BASE}/sphfract | c-ray-mt -t 32 > /dev/null
+cat ${BASE}/sphfract | c-ray-f -s 1024x768 -r 8 > /dev/null
+cat ${BASE}/sphfract | c-ray-mt -t 32 -s 1024x768 -r 8 > /dev/null
+cat ${BASE}/scene | c-ray-f -s 7500x3500 > /dev/null
+cat ${BASE}/scene | c-ray-mt -t 32 -s 7500x3500 > /dev/null
+
