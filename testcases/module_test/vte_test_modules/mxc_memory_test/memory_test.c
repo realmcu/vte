d9d8277330edbcd02995d7fff56583ac18596b17
Hake Huang <b20222@shlx12.ap.freescale.net>
2011-06-07 08:07:01 +0000

    ltp-vte:module-memory-test fix free_page size error
    Signed-off-by: Hake Huang <b20222@shlx12.ap.freescale.net>

--- testcases/module_test/vte_test_modules/mxc_memory_test/memory_test.c
+++ testcases/module_test/vte_test_modules/mxc_memory_test/memory_test.c
@@ -15,29 +15,32 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/list.h>
+#include <linux/types.h>
 #include <linux/kernel.h> 
 #include <linux/interrupt.h>
+#include <linux/moduleparam.h>
+#include <linux/io.h>
 #include <linux/slab.h>
 #include <linux/vmalloc.h>
 #include <linux/sched.h>
+#include <asm/atomic.h>
+#include <mach/hardware.h>
 
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Freescale Semiconductor, Inc.");
 
 static unsigned vmsize = (1024*1024);
-module_param(vmsize, uint, 0);
-MODULE_PARM_DESC(vmsize, "tested size of memory");
+module_param(vmsize,uint,0);
+MODULE_PARM_DESC(vmsize,"tested size of memory");
 
 #define kmalloc_size (128 * 1024 - 16)
 #define freepage_order 9
+#define test_count 1000
 
 static struct timeval start, finish;
 
-static long calc_time(int size)
+static long calc_time(unsigned long size)
 {
 	long ms, speed;
 
@@ -50,7 +53,7 @@ static long calc_time(int size)
 
 static int test_init(void)
 {
-	 int i = 0, count = 10;
+	 int i = 0, count = test_count;
    unsigned long kernel_memaddr = 0; 
    unsigned long speed = 0; 
 	 printk(KERN_INFO "memory test start\n");
@@ -72,7 +75,7 @@ static int test_init(void)
   	}while(--i);
   }
 	do_gettimeofday(&finish);
-  speed = calc_time(kmalloc_size * 10);
+  speed = calc_time(kmalloc_size * test_count);
   printk(KERN_INFO "kmalloc memroy speed is %ld KB/s\n", speed);  
   kfree((void *)kernel_memaddr);
 /*__get_free_pages */ 
@@ -82,7 +85,7 @@ static int test_init(void)
     printk("Allocate memory failure!\n");
     return -ENOMEM;
 	}
-	count = 10;
+	count = test_count;
   do_gettimeofday(&start);
   while(count--){
   	i = (2^freepage_order)*PAGE_SIZE;
@@ -92,9 +95,9 @@ static int test_init(void)
   	}while(--i);
   }
 	do_gettimeofday(&finish);
-  speed = calc_time((2^freepage_order)*PAGE_SIZE * 10);
-  printk(KERN_INFO "free apge memroy speed is %ld KB/s\n", speed);  
-  free_pages(kernel_memaddr,10);
+  speed = calc_time((2^freepage_order)*PAGE_SIZE * test_count);
+  printk(KERN_INFO "get free apge memroy speed is %ld KB/s\n", speed);  
+  free_pages(kernel_memaddr,freepage_order);
 /*test memory mmap mode*/
 /*
    SetPageReserved(virt_to_page(kernel_memaddr));
@@ -107,5 +110,7 @@ static void test_exit(void)
     return;     
 }
 
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Freescale Semiconductor, Inc.");
 module_init(test_init);
 module_exit(test_exit);
