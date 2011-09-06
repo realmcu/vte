f7db17863aa7756682a00f7e0a8e967b8df29bf9
Hake Huang <b20222@shlx12.ap.freescale.net>
2011-09-06 10:01:04 +0000

    ltp-vte:too-chain add spread for tool chain test
    Signed-off-by: Hake Huang <b20222@shlx12.ap.freescale.net>

---
+++ testcases/vte_tests_suite/tool_chain_test/spread.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <linux/time.h>
+
+uint32_t spread4(uint32_t v) __attribute__((noinline));
+
+uint32_t spread4(uint32_t v)
+{
+   __asm__("and %0, %0, #255\n\t"
+       "orr %0, %0, %0, lsl #8\n\t"
+       "orr %0, %0, %0, lsl #16\n\t"
+       :
+			 : "r" (v)
+       );
+   return v;
+}
+
+int main()
+{
+   struct timespec start, end;
+   clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
+   
+   for (int i = 0; i < 10000; i++)
+     {
+	for (int j = 0; j < 10000; j++)
+	  {
+	     spread4(1);
+	     spread4(22);
+	     spread4(133);
+	     spread4(221);
+	  }
+     }
+
+   clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
+   double elapsed = end.tv_sec - start.tv_sec + (end.tv_nsec - start.tv_nsec)*1e-9;
+   printf("%.6f \n", elapsed);
+   return 0;
+}
