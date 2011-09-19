64986cc0ab77c78d09f0d6d4ab6d90808c061ea9
Hake Huang <b20222@shlx12.ap.freescale.net>
2011-09-19 03:04:39 +0000

    ltp-vte:mmc add sd/mmc transfer case to judge whether dma
    Signed-off-by: Hake Huang <b20222@shlx12.ap.freescale.net>

---
+++ testcases/vte_tests_suite/mmc_tests/mmc_transfer.sh
@@ -0,0 +1,72 @@
+#!/bin/sh -x
+# Copyright (C) 2011 Freescale Semiconductor, Inc. All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# Function:     setup
+#
+# Description:  - Check if required commands exits
+#               - Export global variables
+#               - Check if required config files exits
+#               - Create temporary files and directories
+#
+# Return        - zero on success
+#               - non zero on failure. return value from commands ($RC)
+setup()
+{
+#TODO Total test case
+export TST_TOTAL=1
+
+export TCID="setup"
+export TST_COUNT=0
+RC=0
+
+trap "cleanup" 0
+
+#TODO add setup scripts
+
+return $RC
+}
+
+# Function:     cleanup
+#
+# Description   - remove temporary files and directories.
+#
+# Return        - zero on success
+#               - non zero on failure. return value from commands ($RC)
+cleanup()
+{
+RC=0
+
+#TODO add cleanup code here
+return $RC
+}
+
+setup
+
+#main
+
+rs=$(time -p dd if=/dev/mmcblk0p1 of=/dev/null bs=1M count=10 2>&1 | grep real | awk '{print $2}')
+user=$(time -p dd if=/dev/mmcblk0p1 of=/dev/null bs=1M count=10 2>&1 | grep user | awk '{print $2}')
+sys=$(time -p dd if=/dev/mmcblk0p1 of=/dev/null bs=1M count=10 2>&1 | grep sys | awk '{print $2}')
+
+judge=$(echo "$rs > ( $user + $sys)" | bc)
+if [  $judge -eq 1 ]; then
+  echo "wait time implys there are DMA transfer, test PASS"
+	return 0
+fi
+echo "test FAIL, no DMA involved"
+return 1
+
