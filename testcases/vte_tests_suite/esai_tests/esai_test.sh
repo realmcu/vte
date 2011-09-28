16535b1a6a62ec7c4b4eb1840b7ed068500faa75
Hake Huang <b20222@shlx12.ap.freescale.net>
2011-09-28 07:20:14 +0000

    ltp-vte:esai add 5.1 channel 48000 test
    Signed-off-by: Hake Huang <b20222@shlx12.ap.freescale.net>

--- testcases/vte_tests_suite/esai_tests/esai_test.sh
+++ testcases/vte_tests_suite/esai_tests/esai_test.sh
@@ -73,6 +73,30 @@ fi
 return $RC
 }
 
+test_case_03()
+{
+#TODO give TCID
+TCID="ESAI_5.1_auto_test"
+#TODO give TST_COUNT
+TST_COUNT=1
+RC=0
+
+#print test info
+tst_resm TINFO "test $TST_COUNT: $TCID "
+
+speaker-test -t sine -c6 -s1 || RC=1
+speaker-test -t sine -c6 -s2 || RC=$(echo $RC 2) 
+speaker-test -t sine -c6 -s3 || RC=$(echo $RC 3)
+speaker-test -t sine -c6 -s4 || RC=$(echo $RC 4)
+speaker-test -t sine -c6 -s5 || RC=$(echo $RC 5)
+speaker-test -t sine -c6 -s6 || RC=$(echo $RC 6)
+
+if [ '$RC' != '0' ]; then
+RC=1
+fi
+ 
+return $RC
+}
 
 
 # main function
