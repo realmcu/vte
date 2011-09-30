51680f8b0f42a10d7b15086451ccabbb24f4912d
Hake Huang <b20222@shlx12.ap.freescale.net>
2011-09-30 02:47:08 +0000

    ltp-vte:mmc add emmc mode clock check for ddr
    Signed-off-by: Hake Huang <b20222@shlx12.ap.freescale.net>

---
+++ testcases/vte_tests_suite/mmc_tests/eMMC_mode_test.sh
@@ -0,0 +1,55 @@
+#!/bin/sh -x
+
+# Function:     setup
+#
+# Description:  - Check if required commands exits
+#               - Export global variables
+#               - Check if required config files exits
+#               - Create temporary files and directories
+#
+# Return        - zero on success
+#               - non zero on failure. return value from commands ($RC)
+setup()
+{
+#TODO Total test case
+export TST_TOTAL=1
+
+export TCID="setup"
+export TST_COUNT=0
+RC=0
+
+trap "cleanup" 0
+
+#TODO add setup scripts
+dbg=$(mount | grep debugfs | wc -l)
+if [ $dbg -eq 0 ]; then
+ mount -t debugfs none /sys/kernel/debug
+fi
+
+return $RC
+}
+
+# Function:     cleanup
+#
+# Description   - remove temporary files and directories.
+#
+# Return        - zero on success
+#               - non zero on failure. return value from commands ($RC)
+cleanup()
+{
+RC=0
+
+#TODO add cleanup code here
+echo "clean up..."
+
+return $RC
+}
+
+#main
+setup
+
+clock=$(cat /sys/kernel/debug/mmc1/ios | grep clock | awk '{print $2}')
+
+[ $clock -eq 52000000 ] || RC=1
+
+return $RC
