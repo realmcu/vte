d9d0f181b71b1b581cf9c26e35a1407b5acce913
Hake Huang <b20222@freescale.com>
2012-03-05 15:26:06 +0800

    add usage function
    Signed-off-by: Hake Huang <b20222@freescale.com>

--- build_vte.sh
+++ build_vte.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
+
+usage()
+{
+  echo "how to run"
+  echo "build_vte.sh <platform config file>"
+  echo "current available platcorm config file"
+  echo "mx6x_evk_config"
+}
+
+if [ $# -lt 1 ];then
+ usage
+ exit 1
+fi
+
+
 if [ -z $EDITOR ]; then
 	EDITOR=nano
 	export $EDITOR
