eb81debf8792cc3198cf598a540a1f9ec3e86726
Sally Guo <sally.guo@freescale.com>
2012-03-14 14:37:32 -0400

    update check_errors.sh
    Signed-off-by: Sally Guo <sally.guo@freescale.com>

--- testcases/vte_tests_suite/network_tests/scripts/check_errors.sh
+++ testcases/vte_tests_suite/network_tests/scripts/check_errors.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 
-errors=$(ethtool -S eth0 | grep errors | wc -l)
+errors=$(ifconfig eth0 | grep errors:0 | wc -l)
 overruns=$(ifconfig eth0 | grep overruns:0 | wc -l)
 
-if [ $errors -eq 9 ] && [ $overruns -eq 2 ]; then
+if [ $errors -eq 2 ] && [ $overruns -eq 2 ]; then
 echo PASS
 exit 0
 fi
 
 echo FAIL
-echo error: $errors
-echo overruns: $overruns
+echo "`ifconfig eth0 | grep error`"
 
 exit 255
