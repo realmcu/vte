bd34fd4e86a85392c3905df5c3bd62af81b7262c
Hake Huang <b20222@freescale.com>
2012-05-21 17:52:16 +0800

    ltp-vte:multi_vpu add offscreen vpu 8 instances test
    Signed-off-by: Hake Huang <b20222@freescale.com>
    Change-Id: Ia94b7a61e9d99b2224b978bdbd980aa8cac88ecf

--- testcases/vte_tests_suite/vpu_tests/test_script/vpu_multi_dec_test.sh
+++ testcases/vte_tests_suite/vpu_tests/test_script/vpu_multi_dec_test.sh
@@ -149,11 +149,178 @@ return $RC
 
 }
 
+# Function:     test_case_03
+# Description   - Test if 8 multi decode test
+#  
+test_case_03()
+{
+#TODO give TCID 
+TCID="vpu_8multi_dec_test"
+#TODO give TST_COUNT
+TST_COUNT=1
+RC=0
+
+#print test info
+tst_resm TINFO "test $TST_COUNT: $TCID "
+
+#TODO add function test scripte here
+rm -rf /tmp/out_enc.dat
+
+srcfile=${STREAM_PATH}/video/COASTGUARD_CIF_IJT.yuv
+ESIZE="-w 352 -h 288"
+TSTCMD="/unit_tests/mxc_vpu_test.out"
+echo "test H264 same 8 instances offscreen"
+
+$TSTCMD -E "-i $srcfile $ESIZE -f 2 -o /tmp/out_enc.dat" || return 1
+${TSTCMD} -D "-i /tmp/out_enc.dat -f 2 -w 720 -h 576 -o /dev/null" &
+pID1=$!
+${TSTCMD} -D "-i /tmp/out_enc.dat -f 2 -w 720 -h 576 -o /dev/null" &
+pID2=$!
+${TSTCMD} -D "-i /tmp/out_enc.dat -f 2 -w 720 -h 576 -o /dev/null" &
+pID3=$!
+${TSTCMD} -D "-i /tmp/out_enc.dat -f 2 -w 720 -h 576 -o /dev/null" &
+pID4=$!
+${TSTCMD} -D "-i /tmp/out_enc.dat -f 2 -w 720 -h 576 -o /dev/null" &
+pID5=$!
+${TSTCMD} -D "-i /tmp/out_enc.dat -f 2 -w 720 -h 576 -o /dev/null" &
+pID6=$!
+${TSTCMD} -D "-i /tmp/out_enc.dat -f 2 -w 720 -h 576 -o /dev/null" &
+pID7=$!
+${TSTCMD} -D "-i /tmp/out_enc.dat -f 2 -w 720 -h 576" &
+pID8=$!
+
+wait $pID1 
+iRC1=$?
+wait $pID2
+iRC2=$?
+wait $pID3
+iRC3=$?
+wait $pID4 
+iRC4=$?
+wait $pID5 
+iRC5=$?
+wait $pID6 
+iRC6=$?
+wait $pID7 
+iRC7=$?
+wait $pID8
+iRC8=$?
+RC=$(expr $iRC1 + $iRC2 + $iRC3 + $iRC4 + $iRC5 + $iRC6 + $iRC7 + $iRC8)
+
+rm -f /tmp/out_enc.dat
+if [ $RC -ne 0 ]; then
+  echo "TFAIL: h264 8 instances test fails"
+fi
+
+echo "test MJPEG same 8 instances offscreen"
+
+$TSTCMD -E "-i $srcfile $ESIZE -f 7 -o /tmp/out_enc.dat" || return 2
+${TSTCMD} -D "-i /tmp/out_enc.dat -f 7 -w 720 -h 576 -o /dev/null" &
+pID1=$!
+${TSTCMD} -D "-i /tmp/out_enc.dat -f 7 -w 720 -h 576 -o /dev/null" &
+pID2=$!
+${TSTCMD} -D "-i /tmp/out_enc.dat -f 7 -w 720 -h 576 -o /dev/null" &
+pID3=$!
+${TSTCMD} -D "-i /tmp/out_enc.dat -f 7 -w 720 -h 576 -o /dev/null" &
+pID4=$!
+${TSTCMD} -D "-i /tmp/out_enc.dat -f 7 -w 720 -h 576 -o /dev/null" &
+pID5=$!
+${TSTCMD} -D "-i /tmp/out_enc.dat -f 7 -w 720 -h 576 -o /dev/null" &
+pID6=$!
+${TSTCMD} -D "-i /tmp/out_enc.dat -f 7 -w 720 -h 576 -o /dev/null" &
+pID7=$!
+${TSTCMD} -D "-i /tmp/out_enc.dat -f 7 -w 720 -h 576" &
+pID8=$!
+
+wait $pID1 
+iRC1=$?
+wait $pID2
+iRC2=$?
+wait $pID3
+iRC3=$?
+wait $pID4 
+iRC4=$?
+wait $pID5 
+iRC5=$?
+wait $pID6 
+iRC6=$?
+wait $pID7 
+iRC7=$?
+wait $pID8
+iRC8=$?
+
+RC=$(expr $RC + $iRC1 + $iRC2 + $iRC3 + $iRC4 + $iRC5 + $iRC6 + $iRC7 + $iRC8)
+
+rm -f /tmp/out_enc.dat
+if [ $RC -ne 0 ]; then
+  echo "TFAIL: MJPEG 8 instances test fails"
+fi
+
+echo "test mixed format offscreen"
+$TSTCMD -E "-i $srcfile $ESIZE -f 0 -o /tmp/out_enc1.dat" || return 3
+$TSTCMD -E "-i $srcfile $ESIZE -f 1 -o /tmp/out_enc2.dat" || return 3
+$TSTCMD -E "-i $srcfile $ESIZE -f 2 -o /tmp/out_enc3.dat" || return 3
+#not support 3 then 7
+$TSTCMD -E "-i $srcfile $ESIZE -f 7 -o /tmp/out_enc4.dat" || return 3
+#not support 4 then 7
+$TSTCMD -E "-i $srcfile $ESIZE -f 7 -o /tmp/out_enc5.dat" || return 3
+#not support 5 then 7
+$TSTCMD -E "-i $srcfile $ESIZE -f 7 -o /tmp/out_enc6.dat" || return 3
+#not support 6 then 7
+$TSTCMD -E "-i $srcfile $ESIZE -f 7 -o /tmp/out_enc7.dat" || return 3
+$TSTCMD -E "-i $srcfile $ESIZE -f 7 -o /tmp/out_enc8.dat" || return 3
+
+${TSTCMD} -D "-i /tmp/out_enc1.dat -f 0 -w 720 -h 576 -o /dev/null" &
+pID1=$!
+${TSTCMD} -D "-i /tmp/out_enc2.dat -f 1 -w 720 -h 576 -o /dev/null" &
+pID2=$!
+${TSTCMD} -D "-i /tmp/out_enc3.dat -f 2 -w 720 -h 576 -o /dev/null" &
+pID3=$!
+${TSTCMD} -D "-i /tmp/out_enc4.dat -f 7 -w 720 -h 576 -o /dev/null" &
+pID4=$!
+${TSTCMD} -D "-i /tmp/out_enc5.dat -f 7 -w 720 -h 576 -o /dev/null" &
+pID5=$!
+${TSTCMD} -D "-i /tmp/out_enc6.dat -f 7 -w 720 -h 576 -o /dev/null" &
+pID6=$!
+${TSTCMD} -D "-i /tmp/out_enc7.dat -f 7 -w 720 -h 576 -o /dev/null" &
+pID7=$!
+${TSTCMD} -D "-i /tmp/out_enc8.dat -f 7 -w 720 -h 576" &
+pID8=$!
+
+wait $pID1 
+iRC1=$?
+wait $pID2
+iRC2=$?
+wait $pID3
+iRC3=$?
+wait $pID4 
+iRC4=$?
+wait $pID5 
+iRC5=$?
+wait $pID6 
+iRC6=$?
+wait $pID7 
+iRC7=$?
+wait $pID8
+iRC8=$?
+
+RC=$(expr $RC + $iRC1 + $iRC2 + $iRC3 + $iRC4 + $iRC5 + $iRC6 + $iRC7 + $iRC8)
+
+rm -f /tmp/out_enc*.dat
+if [ $RC -ne 0 ]; then
+  echo "TFAIL: Mixed 8 instances test fails"
+fi
+
+wait
+
+return $RC
+}
+
 
 #TODO check parameter
 if [ $# -ne 1 ]
 then
-echo "usage $0 <1/2>"
+echo "usage $0 <1/2/3>"
 exit 1 
 fi
 
@@ -166,6 +333,9 @@ case "$1" in
 2)
   test_case_02 || exit $RC 
   ;;
+3)
+  test_case_03 || exit $RC 
+  ;;
 *)
 #TODO check parameter
   echo "usage $0 <1>"
@@ -173,10 +343,4 @@ case "$1" in
 esac
 
 tst_resm TINFO "Test PASS"
-
-
-
-
-
-
 
