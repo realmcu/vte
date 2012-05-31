cf794b81b54e4027d3e09a16aa5343d5ddd361bc
Sally Guo <sally.guo@freescale.com>
2012-05-31 14:21:33 -0400

    ltp-vte: hdmi mode switch audio stress test
    Signed-off-by: Sally Guo <sally.guo@freescale.com>
    Change-Id: I1f0fbe09c70086707e18182770d7599c3dca447a

--- testcases/vte_tests_suite/fb_test/hdmi_stress.sh
+++ testcases/vte_tests_suite/fb_test/hdmi_stress.sh
@@ -5,7 +5,6 @@
 #The code contained herein is licensed under the GNU General Public
 #License. You may obtain a copy of the GNU General Public License
 #Version 2 or later at the following locations:
-#!/bin/sh
 #http://www.opensource.org/licenses/gpl-license.html
 #http://www.gnu.org/copyleft/gpl.html
 ##############################################################################
@@ -28,6 +27,10 @@
 
 setup()
 {
+    export TST_TOTAL=1
+    export TCID="setup"
+    export TST_COUNT=0
+    trap "cleanup" 0
     if [ $(cat /proc/cmdline | grep hdmi | wc -l) -eq 1 ]; then
        echo "Already enable HDMI in boot cmdline"
        if [ $(cat /sys/devices/platform/mxc_hdmi/cable_state) = "plugout" ]; then
@@ -38,9 +41,7 @@ setup()
        echo "Not enable HDMI in boot cmdline"
        RC=1
      fi
-       
     return $RC
-       
 }
 
 # Function:     cleanup
@@ -51,7 +52,7 @@ setup()
 #               - non zero on failure. return value from commands ($RC)
 cleanup()
 {
-    RC=0
+   RC=0
 }
 usage()
 {
@@ -65,23 +66,53 @@ TCID="Display mode stress test"
 #TODO give TST_COUNT
 TST_COUNT=1
 RC=1
+echo 0 > /sys/class/graphics/fb0/blank
+echo -e "\033[9;0]" > /dev/tty0 
 #print test info
 tst_resm TINFO "test $TST_COUNT: $TCID "
 i=0
-loops=1000000
+loops=10000000
 while [ $i -lt $loops ]
 do
 	i=`expr $i + 1`
 
         echo S:1920x1080p-60 > /sys/class/graphics/fb0/mode
-        echo 0 > /sys/class/graphics/fb0/blank
+        echo q| fbv $LTPROOT/testcases/bin/butterfly.png
+        cat /sys/class/graphics/fb0/mode
+        echo "times: $i"
+        sleep 4
+
+        echo S:640x480p-60 > /sys/class/graphics/fb0/mode
+        echo q| fbv $LTPROOT/testcases/bin/butterfly.png
+        cat /sys/class/graphics/fb0/mode
+        echo "times: $i"
+        sleep 4
+
+        echo S:1280x720p-50 > /sys/class/graphics/fb0/mode
         echo q| fbv $LTPROOT/testcases/bin/butterfly.png
         cat /sys/class/graphics/fb0/mode
         echo "times: $i"
         sleep 4
 
         echo S:720x480p-60 > /sys/class/graphics/fb0/mode
-        echo 0 > /sys/class/graphics/fb0/blank
+        echo q| fbv $LTPROOT/testcases/bin/butterfly.png
+        cat /sys/class/graphics/fb0/mode
+        echo "times: $i"
+        sleep 4
+
+        echo S:1920x1080p-24 > /sys/class/graphics/fb0/mode
+        echo q| fbv $LTPROOT/testcases/bin/butterfly.png
+        cat /sys/class/graphics/fb0/mode
+        echo "times: $i"
+        sleep 4
+
+        echo S:720x576p-50 > /sys/class/graphics/fb0/mode
+        echo q| fbv $LTPROOT/testcases/bin/butterfly.png
+        cat /sys/class/graphics/fb0/mode
+        echo "times: $i"
+        sleep 4
+
+        echo S:1280x720p-60 > /sys/class/graphics/fb0/mode
         echo q| fbv $LTPROOT/testcases/bin/butterfly.png
         cat /sys/class/graphics/fb0/mode
         echo "times: $i"
@@ -90,6 +121,81 @@ done
 RC=0
 return $RC
 }
+
+test_case_02()
+{
+#TODO give TCID 
+TCID="Audio mode stress test"
+#TODO give TST_COUNT
+TST_COUNT=2
+RC=1
+echo 0 > /sys/class/graphics/fb0/blank
+echo -e "\033[9;0]" > /dev/tty0
+#print test info
+tst_resm TINFO "test $TST_COUNT: $TCID "
+mkdir /mnt/temp
+mount -t tmpfs tmpfs /mnt/temp || exit 4
+FILES="audio192k16S.wav audio176k16S.wav audio96k16S.wav audio88k16S.wav audio48k16S.wav audio44k16S.wav audio32k16S.wav audio11k16S.wav"
+for fname in $FILES
+do
+cp /mnt/nfs/test_stream/alsa_stream/$fname /mnt/temp
+done
+num=`aplay -l |grep -i "imxhdmisoc" |awk '{ print $2 }'|sed 's/://'`
+
+i=0
+loops=10000000
+while [ $i -lt $loops ]
+do
+        i=`expr $i + 1`
+
+        echo S:1920x1080p-60 > /sys/class/graphics/fb0/mode
+        aplay -Dplughw:$num -M /mnt/temp/audio11k16S.wav
+        cat /sys/class/graphics/fb0/mode
+        echo "times: $i"
+        sleep 4
+
+        echo S:640x480p-60 > /sys/class/graphics/fb0/mode
+        aplay -Dplughw:$num -M /mnt/temp/audio32k16S.wav
+        cat /sys/class/graphics/fb0/mode
+        echo "times: $i"
+        sleep 4
+
+        echo S:1280x720p-50 > /sys/class/graphics/fb0/mode
+        aplay -Dplughw:$num -M /mnt/temp/audio44k16S.wav
+        cat /sys/class/graphics/fb0/mode
+        echo "times: $i"
+        sleep 4
+
+        echo S:720x480p-60 > /sys/class/graphics/fb0/mode
+        aplay -Dplughw:$num -M /mnt/temp/audio48k16S.wav
+        cat /sys/class/graphics/fb0/mode
+        echo "times: $i"
+        sleep 4
+
+        echo S:1920x1080p-24 > /sys/class/graphics/fb0/mode
+        aplay -Dplughw:$num -M /mnt/temp/audio88k16S.wav
+        cat /sys/class/graphics/fb0/mode
+        echo "times: $i"
+        sleep 4
+
+        echo S:720x576p-50 > /sys/class/graphics/fb0/mode
+        aplay -Dplughw:$num -M /mnt/temp/audio176k16S.wav
+        cat /sys/class/graphics/fb0/mode
+        echo "times: $i"
+        sleep 4
+
+        echo S:1280x720p-60 > /sys/class/graphics/fb0/mode
+        aplay -Dplughw:$num -M /mnt/temp/audio192k16S.wav
+        cat /sys/class/graphics/fb0/mode
+        echo "times: $i"
+        sleep 4
+
+done
+umount /mnt/temp
+RC=0
+return $RC
+}
+
 # Function:     main
 #
 # Description:  - Execute all tests, exit with test status.
@@ -100,6 +206,9 @@ setup || exit $RC
 case "$1" in
 1)
   test_case_01 || exit 2
+  ;;
+2)
+  test_case_02 || exit 3
   ;;
 *)
   usage
