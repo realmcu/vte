4e58ef41bd6ef2fb61f5bfbe97b78ee53233b656
Spring Zhang <spring.zhang@freescale.com>
2012-06-25 17:52:01 +0800

    v4l-correct issues in module probe up script
    Signed-off-by: Spring Zhang <spring.zhang@freescale.com>

--- testcases/vte_tests_suite/tools/v4l_module.sh
+++ testcases/vte_tests_suite/tools/v4l_module.sh
@@ -1,8 +1,9 @@
-#!/bin/sh -x
+#!/bin/sh
 # will check the CAMERA env as the deafult camera
 # possbile CAMERA are ov3640, ov5640, ov5642
 
 camera_module=
+
 NO_REMOVE='Y'
 
 rmodule()
@@ -24,7 +25,9 @@ check_platform_camera()
  bus_if=$(ls -d /sys/devices/platform/imx-i2c.*)
  #if there are multi camera support then choice the given one or last one
  for j in $bus_if; do
+    set -x
 	names=$(find $j -name name | xargs grep ov)
+    set +x
 	for i in $names ; do
 		apd=
 		camera_all=$(echo $i | cut -d ':' -f 2)
@@ -34,29 +37,29 @@ check_platform_camera()
 			apd=_${appends}
 		fi
 		if [ $find -eq 1  ]; then
-			rmodule  ${camera}_camera${apd}
+			rmodule ${camera}_camera${apd}
 			continue
 		fi
 		if [ ! -z "$CAMERA"  ];then
 			if [ "$CAMERA" = ${camera}${apd} ]; then
 				camera_module=${camera}_camera${apd}
-				rmodule  ${camera}_camera${apd}
+				rmodule ${camera}_camera${apd}
 				find=1
 			else
 				camera_module=
-				rmodule  ${camera}_camera${apd}
+				rmodule ${camera}_camera${apd}
 			fi
 		else
-			rmodule  ${camera}_camera${apd}
+			rmodule ${camera}_camera${apd}
 			sleep 1
 			camera_module=$(echo $camera_module  ${camera}_camera${apd})
 		fi
 	done
  done
 
- rmodule  mxc_v4l2_capture || return 1
+ rmodule mxc_v4l2_capture || return 1
 
- if [ -z $camera_module ]; then
+ if [ -z "$camera_module" ]; then
 	return 1
  else
  	return 0
@@ -79,6 +82,8 @@ v4l_setup()
    #turn on the display
    echo 0 > /sys/class/graphics/fb0/blank
    sleep 1
+
+   set -x
    #keep cursor on
    echo -e "\033[9;0]" > /dev/tty0
    for cm in $camera_module; do
@@ -87,14 +92,17 @@ v4l_setup()
    sleep 1
    modprobe mxc_v4l2_capture || return 1
    sleep 1
+   set +x
+
    retry=5
    while [ ! -e /dev/video0 ]; do
 	   sleep 1
 	   if [ $retry -eq 0 ]; then
 		   return 1;
-		fi
-	retry=$(expr $retry - 1)
+	   fi
+   retry=$(expr $retry - 1)
    done
+
    return 0
 }
 
@@ -107,12 +115,15 @@ v4l_cleanup()
 	if [ $RC -ne 0 ]; then
   		echo "can not find support ovCameras for this platform"
   		exit 1
-	fi
-   for cm in $camera_module; do
-   rmodule  $cm
-   done
-   sleep 2
-   rmodule  mxc_v4l2_capture || return 1
+    fi
+
+    set -x
+    for cm in $camera_module; do
+        rmodule $cm
+    done
+    sleep 2
+    rmodule mxc_v4l2_capture || return 1
+    set +x
 }
 
 
@@ -121,11 +132,11 @@ RC=0
 
 echo $CAMERA
 
-if [ -z $CAMERA ]; then
-	if [ "$DUAL" == 2  ]  ; then
+if [ -z "$CAMERA" ]; then
+	if [ "$DUAL" = "2"  ]  ; then
   		echo "define default"
   		CAMERA=ov5640
-	elif [ "$DUAL" == 3 ] ; then
+	elif [ "$DUAL" = "3" ] ; then
 		echo "will probe all registed camera"
 	else
 		pt=$(platfm.sh)
@@ -161,8 +172,9 @@ if [ $RC  -ne 0  ];then
   exit $RC
 else
   echo "OK to $1 v4l"
-  if [ "$( echo $camera_module | grep ov5642_camera)" ] && [ $1 == setup ]; then
-	  #special for ov5642 which only support some modes in 6q Lite platform
+  if echo $camera_module | grep ov5642_camera && [ "$1" = "setup" ]
+  then
+	  #special for ov5642 which only support some modes on 6q Lite platform
 	  return 56
   else
   exit $RC 
