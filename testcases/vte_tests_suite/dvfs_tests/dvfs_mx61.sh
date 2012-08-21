88beae56d91915ce7a6042d0ff37b5e5c09ab954
Hake Huang <b20222@freescale.com>
2012-08-21 20:28:50 +0800

    ltp-vte:dvfs fix a bug for array
    Signed-off-by: Hake Huang <b20222@freescale.com>

--- testcases/vte_tests_suite/dvfs_tests/dvfs_mx61.sh
+++ testcases/vte_tests_suite/dvfs_tests/dvfs_mx61.sh
@@ -413,7 +413,8 @@ pre_bus_mode()
 	axi_path=$(find /sys/kernel/debug/clock/osc_clk -name axi_clk)
 	ddr_path=$(find /sys/kernel/debug/clock/osc_clk -name mmdc_ch0_axi_clk)
 	a_stream_path=/mnt/nfs/test_stream/video/ToyStory3_H264HP_1920x1080_10Mbps_24fps_AAC_48kHz_192kbps_2ch_track1.h264
-	platfm=$(platfm.sh)
+	platfm.sh
+	platfm=$?
     mount -t tmpfs tmpfs /tmp
 	cp ${LTPROOT}/testcases/bin/rtc_testapp_6 /tmp/
 	cp /mnt/nfs/test_stream/alsa_stream/audio12k16M.wav /tmp/
@@ -437,32 +438,32 @@ declare -a axi;
 declare -a ddr;
 
 if [ $platfm -eq 63 ]; then
-  axi="24000000 132000000 264000000 264000000"
-  ddr="24000000 50000000  396000000 528000000"
+  axi=("24000000" "132000000" "264000000" "264000000");
+  ddr=("24000000" "50000000"  "396000000" "528000000");
 elif [ $platfm -eq 61  ];then
-  axi="24000000 132000000 264000000 264000000"
-  ddr="24000000 50000000 396000000 396000000"
+  axi=("24000000" "132000000" "264000000" "264000000");
+  ddr=("24000000" "50000000" "396000000" "396000000");
 elif [ $platfm -eq 60 ]; then
-  axi="24000000 198000000 198000000 198000000"
-  ddr="24000000 396000000 396000000 396000000"
+  axi=("24000000" "198000000" "198000000" "198000000");
+  ddr=("24000000" "396000000" "396000000" "396000000");
 else
-  axi="24000000 132000000 264000000 264000000"
-  ddr="24000000 50000000  396000000 528000000"
+  axi=("24000000" "132000000" "264000000" "264000000");
+  ddr=("24000000" "50000000"  "396000000" "528000000");
 fi
 
 case "$1" in
 low)
 #axi bus to 24M
-     [ axi[0] -eq 24000000 ] || RC=1
+     [ ${axi[0]} -eq 24000000 ] || RC=1
     ;;
 audio)
-     [ ddr[1] -eq 50000000 ] || RC=2
+     [ ${ddr[1]} -eq 50000000 ] || RC=2
     ;;
 medium)
-    [ ddr[2] -eq  396000000 ] || RC=3
+    [ ${ddr[2]} -eq  396000000 ] || RC=3
     ;;
 high)
-    [ ddr[3] -eq 528000000 ] || RC=4
+    [ ${ddr[3]} -eq 528000000 ] || RC=4
     ;;
 *)
     ;;
