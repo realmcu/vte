784d527399ec4a4db89bbf89370b15cfc6d50641
Spring Zhang <spring.zhang@freescale.com>
2012-11-15 17:35:22 +0800

    vte Power-add debug msg and delay to avoid error below
    /mnt/nfs/vte_IMX6DL-Sabre-SD/testcases/bin/powerstate_test.sh: line 41: can't create /sys/power/state: nonexistent directory
    
    Signed-off-by: Spring Zhang <spring.zhang@freescale.com>
    Change-Id: Icd7216c3bc92b3dbdc4ff85a0fdde71322fc5c10

--- testcases/vte_tests_suite/power_manager/powerstate_test.sh
+++ testcases/vte_tests_suite/power_manager/powerstate_test.sh
@@ -5,12 +5,12 @@ loops=500
 
 setup()
 {
-trap "clean" 0
+    trap "clean" 0
 }
 
 clean()
 {
-	echo none > /sys/power/pm_test
+    echo none > /sys/power/pm_test
 }
 
 setup
@@ -18,60 +18,75 @@ setup
 #test the freezing of processes, suspending of devices, platform global
 #control methods(*), the disabling of nonboot CPUs and suspending of
 #platform/system devices
-echo "core test"
+echo "===Power state core test==="
 i=0
 echo core > /sys/power/pm_test
 while [ $i -lt $loops ]
-	do
-   i=$(expr $i + 1)
-   echo mem > /sys/power/state
-	 echo standby > /sys/power/state
-	done
+do
+    i=$(expr $i + 1)
+    echo "===Power state core test $i times==="
+    echo mem > /sys/power/state
+    sleep 1
+    echo standby > /sys/power/state
+    sleep 1
+done
 
 #test the freezing of processes, suspending of devices, platform
 #global control methods(*) and the disabling of nonboot CPUs
-echo "processors test"
+echo "===Power state processors test==="
 i=0
 echo processors > /sys/power/pm_test
 while [ $i -lt $loops ]
-	do
-   i=$(expr $i + 1)
-   echo mem > /sys/power/state
-	 echo standby > /sys/power/state
-	done
+do
+    i=$(expr $i + 1)
+    echo "===Power state processors test $i times==="
+    echo mem > /sys/power/state
+    sleep 1
+    echo standby > /sys/power/state
+    sleep 1
+done
 
 #test the freezing of processes, suspending of devices and platform
 #global control methods(*)
-echo "platform test"
+echo "===Power state platform test==="
 i=0
 echo platform > /sys/power/pm_test
 while [ $i -lt $loops ]
-	do
-   i=$(expr $i + 1)
-   echo mem > /sys/power/state
-	 echo standby > /sys/power/state
-	done
+do
+    i=$(expr $i + 1)
+    echo "===Power state platform test $i times==="
+    echo mem > /sys/power/state
+    sleep 1
+    echo standby > /sys/power/state
+    sleep 1
+done
 
 #test the freezing of processes and suspending of devices
-echo "devices test"
+echo "===Power state devices test==="
 i=0
 echo devices > /sys/power/pm_test
 while [ $i -lt $loops ]
-	do
-   i=$(expr $i + 1)
-   echo mem > /sys/power/state
-	 echo standby > /sys/power/state
-	done
+do
+    i=$(expr $i + 1)
+    echo "===Power state devices test $i times==="
+    echo mem > /sys/power/state
+    sleep 1
+    echo standby > /sys/power/state
+    sleep 1
+done
 
 #test the freezing of processes
-echo "freezer test"
+echo "===Power state freezer test==="
 i=0
 echo freezer > /sys/power/pm_test
 while [ $i -lt $loops ]
-	do
-   i=$(expr $i + 1)
-   echo mem > /sys/power/state
-	 echo standby > /sys/power/state
-	done
+do
+    i=$(expr $i + 1)
+    echo "===Power state freezer test $i times==="
+    echo mem > /sys/power/state
+    sleep 1
+    echo standby > /sys/power/state
+    sleep 1
+done
 
-echo "test Pass"
+echo "Test Pass"
