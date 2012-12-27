80c5761a1bbc8bd11528cdbd1c6549e1ddf8d14c
Hake Huang <b20222@freescale.com>
2012-12-27 10:11:19 +0800

    ltp-vte:security add ahash test
    Signed-off-by: Hake Huang <b20222@freescale.com>

--- testcases/vte_tests_suite/security_tests/crypto_scripts/tcryp.sh
+++ testcases/vte_tests_suite/security_tests/crypto_scripts/tcryp.sh
@@ -143,7 +143,7 @@ tst_resm TINFO "test $TST_COUNT: $TCID "
 
 #TODO add function test scripte here
 MODE_LIST="301 302 303 304 305 306 307 308 309 310 \
-           311 312 313 314 315 316 317"
+           311 312 313 314 315 316 317 400"
 
 for i in $MODE_LIST
 do
