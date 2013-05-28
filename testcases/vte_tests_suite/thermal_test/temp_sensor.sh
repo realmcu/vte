847e926395df71b0b220aab764b8813ae452f3df
Lina Jin <b42475@freescale.com>
2013-05-28 16:20:41 +0800

    Thermal-update thermal file interface for 3.5.7
    Signed-off-by: Lina Jin <b42475@freescale.com>

--- testcases/vte_tests_suite/thermal_test/temp_sensor.sh
+++ testcases/vte_tests_suite/thermal_test/temp_sensor.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 mac=$(cat /sys/class/net/eth0/address | sed 's/:/_/g')
+#THERMO_PATH="/sys/devices/soc.0/2000000.aips-bus/21bc4e0.thermal"
 echo "------------------------------------------------------------"
 while true
 do
  sleep 30
  echo "<<<MEASUER_START>>>" >> /root/${mac}
- if [ -e /sys/devices/virtual/thermal/thermal_zone0/temp ]; then
-	cat /sys/devices/virtual/thermal/thermal_zone0/temp >> /root/${mac}
+ if [ -e /sys/devices/soc.0/2000000.aips-bus/21bc4e0.thermal/temp ]; then
+	cat /sys/devices/soc.0/2000000.aips-bus/21bc4e0.thermal/temp >> /root/${mac}
  fi
  top -n 1  | head -n 10 >> /root/${mac}
  date -u >> /root/${mac}
