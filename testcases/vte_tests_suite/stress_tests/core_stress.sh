844270db37c1d82048d3a7f987e7102accc0f62f
Spring Zhang <spring.zhang@freescale.com>
2013-07-15 10:44:48 +0800

    core_stress.sh-fix the issue when no any parameter input
    Signed-off-by: Spring Zhang <spring.zhang@freescale.com>

--- testcases/vte_tests_suite/stress_tests/core_stress.sh
+++ testcases/vte_tests_suite/stress_tests/core_stress.sh
@@ -1,20 +1,29 @@
 #!/bin/sh
-if [ $# -ne 1 ]; then
-echo "need number of process"
-echo "$0 1/2/3/4/"
+if [ $# -le 1 ]; then
+    echo "need number of process"
+    echo "$0 1/2/3/4/"
+    hint_msg="Warning: For no number of process given, CPU stress run on all cpu \
+cores, or please use Ctrl+C to interrupt and input a number."
+    cpu_num=`cat /proc/cpuinfo |grep processor |wc -l`
+else
+    cpu_num=$1
 fi
 
-while [ true ];
-do
-if [ $1 -eq 4  ]; then
-dry2 &
-fi
-if [ $1 -gt 2  ]; then
-dry2 &
-fi
-if [ $1 -gt 1  ]; then
-dry2 &
-fi
-dry2
+
+while [ true ]; do
+    echo
+    echo $hint_msg
+    sleep 10
+
+    if [ $cpu_num -eq 4  ]; then
+    dry2 &
+    fi
+    if [ $cpu_num -gt 3  ]; then
+    dry2 &
+    fi
+    if [ $cpu_num -gt 2  ]; then
+    dry2 &
+    fi
+    dry2
 done
 
